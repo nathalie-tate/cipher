784f8becb5dfb0b2b3d2e62e5e43f5cf6ab59d1a
Nathalie <nrt@nathalietate.xyz>
2022-10-09 16:10:32 -0400

    add rot13 to runthemall

--- runthemall.pl
+++ runthemall.pl
@@ -39,6 +39,17 @@ for my $text (@ciphertext)
 {
     say "CIPHER TEXT: $text";
 
+    say "ROT13";
+    {
+      my $result = rot13($text);
+      my $flag = "";
+      for (split / /, $result)
+      {
+          $flag = "\t" . "*" x 20 if exists $dict{lc $_} && length $_ > 2;
+      }
+      say $result . $flag;
+    }
+
     say "AFFINE";
     for my $a (1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25)
     {
